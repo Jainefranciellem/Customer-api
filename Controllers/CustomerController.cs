dd05dd8ac2dace7c5618211a157ade8f5be7fedf
Jaine Franciellem Santos Dias <jfsdias@fundat3.rima>
2024-09-17 13:51:37 -0300

    Add method GetById

--- Controllers/CustomerController.cs
+++ Controllers/CustomerController.cs
@@ -21,5 +21,12 @@ namespace Controllers
         {
             return await _customerCollection.Find(FilterDefinition<Customer>.Empty).ToListAsync();
         }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(string id)
+        {
+            var costumer = await _customerCollection.Find<Customer>(c => c.Id == id).FirstOrDefaultAsync();
+            return costumer is not null ? (IActionResult)Ok(costumer) : NotFound();
+        }
     }
 }
