4e7a75ecfd58f92a1073565bc54e229ffc5aca3c
Jaine Franciellem Santos Dias <jfsdias@fundat3.rima>
2024-09-18 14:52:23 -0300

    remove files

--- src/app/app.routes.ts
+++
@@ -1,9 +0,0 @@
-import { Routes } from '@angular/router';
-import { ListComponent } from './pages/list/list.component';
-
-export const routes: Routes = [
-    {
-        path: 'list',
-        component: ListComponent
-    }
-];
