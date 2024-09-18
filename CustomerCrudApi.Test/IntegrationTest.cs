1309ef22df37f926392335e588285363dcdfc943
Jaine Franciellem Santos Dias <jfsdias@fundat3.rima>
2024-09-18 10:00:55 -0300

    implements GetById test

--- CustomerCrudApi.Test/IntegrationTest.cs
+++ CustomerCrudApi.Test/IntegrationTest.cs
@@ -13,11 +13,20 @@ public class IntegrationTest: IClassFixture<WebApplicationFactory<Program>>
     }
     
 
-    [Theory(DisplayName = "Testando a rota /GET Clients")]
+    [Theory(DisplayName = "Testando a rota /GET Customers")]
     [InlineData("/api/Customer")]
     public async Task TestGetCustomers(string url)
     {
         var response = await _clientTest.GetAsync(url);
         Assert.Equal(System.Net.HttpStatusCode.OK, response?.StatusCode);
+    }
+
+    [Theory(DisplayName = "Testando a rota /GET Customer/{id}")]
+    [InlineData("/api/Customer/{id}")]
+    public async Task TestGetCustomerById(string url)
+    {
+        var testId = "6508e7f5e4b08d4e87e4f1c8";
+        var response = await _clientTest.GetAsync(url.Replace("{id}", testId));
+        Assert.True(response.StatusCode == System.Net.HttpStatusCode.OK || response.StatusCode == System.Net.HttpStatusCode.NotFound);
     }
 }
